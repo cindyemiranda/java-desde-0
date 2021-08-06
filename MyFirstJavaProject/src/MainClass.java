d41100d752ee06b2af0d64ddd8e906d38978554a
cindyemiranda <cindy.miranda0310@gmail.com>
2021-08-06 14:28:10 -0500

    Types of variables in java

--- MyFirstJavaProject/src/MainClass.java
+++ MyFirstJavaProject/src/MainClass.java
@@ -4,6 +4,24 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("I like fried Chicken!!!");
 		System.out.println("It tastes really good!!!");
+		
+		//Asignements
+		
+		int x = 123; // declarations
+		long xl= 1928292829282928232L;
+		float f=3.14f;
+		double d=12.322322322;
+		char symbol= '@';
+		String name="My name";
+				
+		System.out.println("My number is: "+x);
+		System.out.println("My long number is: "+xl);
+		System.out.println("My float number is: "+f);
+		System.out.println("My double number is: "+d);
+		System.out.println("My symbol is: "+symbol);
+		System.out.println("Hello, my name is: "+name);
+		
+		
 
 	}
 
