7240faa8e298c87f69a9e38faee0753de328cada
cindyemiranda <cindy.miranda0310@gmail.com>
2021-08-06 12:33:24 -0500

    Java desde 0, funcion main

---
+++ MyFirstJavaProject/src/MainClass.java
@@ -0,0 +1,10 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("I like fried Chicken!!!");
+		System.out.println("It tastes really good!!!");
+
+	}
+
+}
