33f7252ceb3a8c81a51cd0b8f8a09f57d030dfe7
cindyemiranda <cindy.miranda0310@gmail.com>
2021-08-06 20:19:22 -0500

    Tutorial #3 Swap two variables and Tutorial #4 User input

--- MyFirstJavaProject/src/MainClass.java
+++ MyFirstJavaProject/src/MainClass.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("I like fried Chicken!!!");
 		System.out.println("It tastes really good!!!");
-		
+		System.out.println();
+		System.out.println();
+		/**
+		 * Tutorial #2 Variables
+		 */
 		//Asignements
 		
 		int x = 123; // declarations
@@ -20,8 +25,42 @@ public class MainClass {
 		System.out.println("My double number is: "+d);
 		System.out.println("My symbol is: "+symbol);
 		System.out.println("Hello, my name is: "+name);
+		System.out.println();
+		System.out.println();
+		/**
+		 * Tutorial #3 Swap two variable
+		 */
+		//Asignements
+		String s_water="water";
+		String s_kool_aid= "Kool-aid";
+		String temp;
 		
+		temp=s_water;
+		s_water=s_kool_aid;
+		s_kool_aid=temp;
 		
+		System.out.println("s_water is: "+s_water);
+		System.out.println("s_kool_aid is : "+s_kool_aid);
+		System.out.println();
+		System.out.println();
+		/**
+		 * Tutorial #4 User Input
+		 */
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("What is your name?");
+		String name_input =scanner.nextLine();
+		
+		System.out.println("How old are you?");
+		int age = scanner.nextInt(); //Only receives a integer
+		scanner.nextLine();
+		
+		System.out.println("What is your favorite food?");
+		String food= scanner.nextLine();
+		
+		System.out.println("Hello "+name_input+"!!");
+		System.out.println("You are "+age+" years old.");
+		System.out.println("You like "+food+" !!");
 
 	}
 
